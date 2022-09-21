8862021b0998c49205d024b9627f3ada1c101162
Pablo Hebenstreit Gotelli <pablo2e@yahoo.com>
2022-09-21 10:17:18 +0200

    feat: empty item component

---
+++ src/test/Item.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import { Item } from '../infrastructure/components/Item';
+
+test('Should render Item component', () => {
+    render(<Item />);
+    expect(screen.getByText('Item')).toBeInTheDocument();
+});
