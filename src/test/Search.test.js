94c39eb63e7be3ab03f107bceaa89551cda66c00
Pablo Hebenstreit Gotelli <pablo2e@yahoo.com>
2022-09-21 10:09:11 +0200

    feat: empty search component

---
+++ src/test/Search.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import { Search } from '../infrastructure/components/Search';
+
+test('Should render Search component', () => {
+    render(<Search />);
+    expect(screen.getByText('Search')).toBeInTheDocument();
+});
